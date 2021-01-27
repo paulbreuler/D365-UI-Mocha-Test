d8f0e0091ff1f8871582fb67bdbb6f73c84075a3
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2021-01-27 13:22:17 -0500

    Added readme, updated tests to support promises, and increased command line exec timeout for Mocha

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # D365-UI-Mocha-Test
+
+Tests can be launched using VS Code's built-in debugger allowing you to set breakpoints in VS Code.
+
+Tests can alternately be ran from the command line using the following command.
+``` bash
+npm run test 
+``` 
--- tests/basic-ui.test.ts
+++ tests/basic-ui.test.ts
@@ -2,10 +2,14 @@
 import {
     expect
 } from 'chai';
+import * as chai from 'chai'
 import { XrmUiTest } from "d365-ui-test";
 import * as fs from "fs";
 import * as playwright from "playwright";
 import * as path from "path";
+
+import chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
 
 const xrmTest = new XrmUiTest();
 let browser: playwright.Browser = null;
@@ -26,25 +30,24 @@ describe('Power Apps Basic UI Tests', function () {
                 '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"'
             ]
         });
-        
+
         browser = response[0];
         context = response[1];
         page = response[2];
-        
+
     })
 
-    it("Should start D365", async function () {
+    it("Should start D365", async () => {
         const settingsPath = path.join(__dirname, "./settings.txt");
         const settingsFound = fs.existsSync(settingsPath);
-        const config = settingsFound ? fs.readFileSync(settingsPath, {encoding: 'utf-8'}) : `${process.env.CRM_URL ?? ""},${process.env.USER_NAME ?? ""},${process.env.USER_PASSWORD ?? ""},${process.env.MFA_SECRET ?? ""}`;
-        
+        const config = settingsFound ? fs.readFileSync(settingsPath, { encoding: 'utf-8' }) : `${process.env.CRM_URL ?? ""},${process.env.USER_NAME ?? ""},${process.env.USER_PASSWORD ?? ""},${process.env.MFA_SECRET ?? ""}`;
+
         const [url, user, password, mfaSecret] = config.split(",");
 
-        let response = await xrmTest.open(url, { userName: user, password: password, mfaSecret: mfaSecret ?? undefined });
-        console.log(response);
+        await xrmTest.open(url, { userName: user, password: password, mfaSecret: mfaSecret ?? undefined });
     });
 
-    this.afterAll(()=>{
+    this.afterAll(() => {
         return xrmTest.close();
     })
 
